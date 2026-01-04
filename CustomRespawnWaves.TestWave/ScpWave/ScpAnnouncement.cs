d87bde1bcd735d423844681cffe21691a8987537
SlejmUr <slejmur@protonmail.com>
2026-01-04 17:19:51 +0100

    Update to latest

--- CustomRespawnWaves.TestWave/ScpWave/ScpAnnouncement.cs
+++ CustomRespawnWaves.TestWave/ScpWave/ScpAnnouncement.cs
@@ -1,20 +1,17 @@
 ﻿using Respawning.Announcements;
 using Subtitles;
 using System.Text;
-using Utils.Networking;
 
 namespace CustomRespawnWaves.TestWave.ScpWave;
 
 public class ScpAnnouncement : WaveAnnouncementBase
 {
-    public override void CreateAnnouncementString(StringBuilder builder)
+    public override void CreateAnnouncement(StringBuilder builder, List<ReferenceHub> spawnedPlayers, out SubtitlePart[] subtitles)
     {
         builder.Append("Security Alert . Substantial SCP Activity Detected . All Personnel is in danger");
-    }
-
-    public override void SendSubtitles()
-    {
-        // Subtitle here. dont know how to do valid one. sorry.
-        //new SubtitleMessage([new SubtitlePart(SubtitleType.Custom, ["security alert . substantial scp activity detected . all personnel is in danger."])]).SendToAuthenticated();
+        subtitles =
+        [
+            new SubtitlePart(SubtitleType.Custom, ["security alert . substantial scp activity detected . all personnel is in danger."])
+        ];
     }
 }
